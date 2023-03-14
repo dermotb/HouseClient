a3ce65530f7bf2bf3f4c4a65a116841cf2ec9268
Dermot Boyle <dermotb@hotmail.com>
2023-03-14 19:57:31 +0000

    Add project files.

---
+++ HouseClient/Models/Property.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace House4U.Models
+{
+    public class Property
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Range(1, int.MaxValue)]
+        public int NumRooms { get; set; }
+        [Required]
+        public bool FullyDelegated { get; set; }
+        [Required]
+        [EmailAddress]
+        public string ClientMail { get; set; }
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("ID: {0} | Address: {1} | Rooms: {2} | Delegated: {3} | Mail: {4}| Expiry: {5}", ID, Address, NumRooms, FullyDelegated, this.ClientMail, this.ExpiryDate);
+        }
+    }
+
+
+}
---
+++ HouseClient/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using House4U.Models;
+using System.Net.Http.Json;
+
+
+namespace Properties
+{
+    public class Program
+    {
+        static HttpClient client { get; set; }
+
+        static void Main(string[] args) 
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5148/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            GetAllProperties().Wait();
+            GetAProperty(1).Wait();
+            GetAllEmails().Wait();
+        }
+
+        private static async Task GetAllEmails()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("***GetAllEmails***");
+            Console.WriteLine("");
+
+            HttpResponseMessage msg = await client.GetAsync("AllEmails");
+
+            if (msg.IsSuccessStatusCode)
+            {
+                IEnumerable<String> mails = await msg.Content.ReadFromJsonAsync<IEnumerable<String>>();
+                foreach (string m in mails)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+            else
+            {
+                Console.WriteLine("GetAllEmails - Error: " + msg.StatusCode + msg.ReasonPhrase);
+            }
+        }
+
+        private static async Task GetAProperty(int v)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("***GetAProperty***");
+            Console.WriteLine("");
+
+            HttpResponseMessage msg = await client.GetAsync("api/Property/0");
+
+            if (msg.IsSuccessStatusCode)
+            {
+                Property prop = await msg.Content.ReadFromJsonAsync<Property>();
+                Console.WriteLine(prop);
+
+            }
+            else
+            {
+                Console.WriteLine("GetAProperty - Error: " + msg.StatusCode + msg.ReasonPhrase);
+            }
+        }
+
+        private static async Task GetAllProperties()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("***GetAllProperties***");
+            Console.WriteLine("");
+
+            HttpResponseMessage msg = await client.GetAsync("api/Property");
+
+            if (msg.IsSuccessStatusCode)
+            {
+                IEnumerable<Property> props = await msg.Content.ReadFromJsonAsync<IEnumerable<Property>>();
+                foreach (Property p in props)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Default Get - Error: "+msg.StatusCode+msg.ReasonPhrase);
+            }
+
+        }
+    }
+}
